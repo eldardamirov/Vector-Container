5cb9841a3582a67b12a727e3aa8f015bf6f8b7fc
eldardamirov <eldardamirov75@mail.ru>
2018-10-20 22:47:50 +0300

    Initial Commit

---
+++ Vector Container/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vector Container
+//
+//  Created by Эльдар Дамиров on 20.10.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
